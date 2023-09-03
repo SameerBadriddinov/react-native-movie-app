ef35f82ef7c06269ae87e39db0545c799456741e
Samar Badriddinov <99094663+SameerBadriddinov@users.noreply.github.com>
2023-09-03 11:22:11 +0300

    #3. Tab navigation

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import AppNavigation from './src/navigations/app.navigation';
+import TabNavigation from './src/navigations/tab.navigation';
 import './src/styles/global.css';
 
 export default function App() {
-	return <AppNavigation />;
+	return <TabNavigation />;
 }
