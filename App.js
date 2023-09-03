a61f49deb6390e10f91c9f3fd059279dc2807609
Samar Badriddinov <99094663+SameerBadriddinov@users.noreply.github.com>
2023-09-03 12:08:50 +0300

    #5. Navbar and Content scroll view

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
-import TabNavigation from './src/navigations/tab.navigation';
+import AppNavigation from './src/navigations/app.navigation';
 import './src/styles/global.css';
 
 export default function App() {
-	return <TabNavigation />;
+	return <AppNavigation />;
 }
--- src/screens/home.js
+++ src/screens/home.js
@@ -1,14 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Button, Text, View } from 'react-native';
+import { Image, ScrollView, View } from 'react-native';
+import { MagnifyingGlassIcon } from 'react-native-heroicons/outline';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Home({ navigation }) {
 	return (
-		<View className='flex-1 items-center justify-center bg-slate-500'>
-			<Text>Home</Text>
-			<Button
-				title='Go to Details'
-				onPress={() => navigation.navigate('Details')}
-			/>
+		<View className='flex-1 bg-slate-950'>
+			<SafeAreaView>
+				<StatusBar style='light' />
+				<View
+					className={
+						'flex-row justify-between items-center mx-4 border-b-2'
+					}
+				>
+					<Image source={require('../../assets/logo.png')} />
+					<MagnifyingGlassIcon
+						size={30}
+						strokeWidth={2}
+						color={'white'}
+					/>
+				</View>
+			</SafeAreaView>
+
+			<ScrollView
+				showsVerticalScrollIndicator={false}
+				contentContainerStyle={{ paddingBottom: 20 }}
+			></ScrollView>
 		</View>
 	);
 }
